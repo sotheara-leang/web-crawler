1103b2cdf1544acd1f6de11060e4dab550b300b3
sotheara <leangsotheara@gmail.com>
2020-08-01 20:33:58 +0700

    refactor codes

--- text_clean.py
+++ text_clean.py
@@ -1,14 +1,34 @@
 import re
 import os
+from tqdm import tqdm
+
 from text_normalize import remove_url, remove_words_w_bracket, normalize
 
 lek_to_tag = re.compile('([\u1780-\u17e9]+) ([\u1780-\u17e9]+) ៗ')
 
 CONT_SEP = '|@@@|'
-CONT_SEP_SEG = '|@ @@|'
+CONT_SEP_SEG = r'\| ?@ ?@ ?@ ?\|'
 
-FILTERS_REGX = {
-    r"^\s+": ''
+ENG_WORD_PATTERN = r" [,.'#’→^<>!?()\w]+( [,.'#’→^<>!?()\w]+)+ "  # we leave in the text only english name
+
+SENT_SEP = '។'
+OTHER_SENT_SEP = ['៕', '?']
+
+BEFORE_FILTER = {
+    r'RFA( ?/ ?[a-zA-Z]+)+': '',
+    r'(រូប ៖ )?RFA ': '',
+}
+
+AFTER_FILTER = {
+    r'^RFA': '',
+    r'Photo': '',
+    r'PhotoProvided': '',
+    r'Provided': '',
+    r'PhotoScreenshot': '',
+    r'RFAPhoto': '',
+    r'VideoPlatformVideoManagementVideoSolutionsVideoPlayer': '',
+    r'YO': '',
+    r'^\w+$': ''
 }
 
 def extract_text_for_segment(file_in, file_out):
@@ -40,19 +60,32 @@ def clean_text(file_in, file_out):
     # read text
     lines = []
     with open(file_in, 'r', encoding='utf-8') as reader:
-        for line in reader:
-            parts = line.split(CONT_SEP_SEG)
+        for line in tqdm(reader):
+            parts = re.split(CONT_SEP_SEG, line)
             for text in parts:
-                # normalize text
-                text = normalize(text, clean_num=False)
-
                 # split sents
-                text = text.replace('៕', '។')
-                sents = text.split('។')
+                for sep in OTHER_SENT_SEP:
+                    text = text.replace(sep, SENT_SEP)
+                
+                # split sents
+                sents = text.split(SENT_SEP)
                 for sent in sents:
-                    lines.append(sent)
+                    # before filter
+                    for k, v in BEFORE_FILTER.items():
+                        sent = re.sub(k, v, sent)
 
-            break
+                    # normalize text
+                    sent = re.sub(ENG_WORD_PATTERN, '', sent)
+                    sent = normalize(sent, clean_num=False)
+                    
+                    # after filter
+                    for k, v in AFTER_FILTER.items():
+                        sent = re.sub(k, v, sent)
+                    
+                    if sent == '':
+                        continue
+                    
+                    lines.append(sent)
 
     # write
     with open(file_out, 'w', encoding='utf-8') as writer:
@@ -68,14 +101,17 @@ def clean_text(file_in, file_out):
             writer.write('%s\n' % line)
 
 def clean_text_corpus(corpus_dir, output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+        
     for module in os.listdir(corpus_dir):
         module_file = os.path.join(corpus_dir, module)
-        if module.startswith('.') or os.path.isdir(module_file):
+        if os.path.isdir(module_file) or module.startswith('.') or module.endswith('.w'):
             continue
 
         print('Processing %s ...' % module)
 
-        output_file = '%s/%s.txt' % (output_dir, module)
+        output_file = '%s/%s' % (output_dir, module)
         clean_text(module_file, output_file)
 
 def extract_lek_to(corpus_dir, file_out):
@@ -106,8 +142,9 @@ def extract_lek_to(corpus_dir, file_out):
         for record in records:
             writer.write('%s\n' % record)
 
-
 if __name__ == '__main__':
     # extract_lek_to('work/raw/rfa', 'work/lek_to.txt')
     # extract_text_for_segment_corpus('work/raw/rfa', 'work/extract/rfa')
-    clean_text('work/segment/rfa/economy.txt.c', 'work/clean/rfa/economy.c')
+    # clean_text('work/segment/rfa/politics.txt.c', 'work/clean/rfa/politics.txt.c')
+    # clean_text('work/test.txt', 'work/clean/rfa/economy.c')
+    clean_text_corpus('work/segment/rfa', 'work/clean/rfa')
