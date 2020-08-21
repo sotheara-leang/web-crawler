d11095dd2a3d2c46091bd2d31c30614232168618
sotheara <leangsotheara@gmail.com>
2020-08-21 23:05:14 +0700

    refactor codes

--- mp4_to_wav.sh
+++ mp4_to_wav.sh
@@ -6,6 +6,6 @@ mkdir -p $output
 for f in $input/*.mp4
 do
   echo "Converting ${f}..."
-  filename=`basename $f .mp3`
-  ffmpeg -i $f -acodec pcm_u16 -ar 16000 -ac 1 -f wav $output/$filename.wav
+  filename=`basename ${f%%.*}`
+  ffmpeg -i $f -ar 16000 -ac 1 -f wav $output/$filename.wav
 done
